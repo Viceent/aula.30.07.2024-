1998cca473bb861659d55e6b7977b2b8bb0059d4
Viceent <vicente04046@gmail.com>
2024-08-06 21:31:50 -0300

    aluno.controller

---
+++ WebApplication1/Controllers/Aluno.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Aluno : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
